e05f276b7e6ed0f410cc55b77ec4d4d509f19936
xyms6 <luis.surf2000@gmail.com>
2024-12-05 14:40:15 -0300

    Delete Downloads/INTEGRADOR2-main/INTEGRADOR2-main/projeto-integradorDeploy-main/projeto-integradorDeploy-main/projeto-interador-main directory

--- Downloads/INTEGRADOR2-main/INTEGRADOR2-main/projeto-integradorDeploy-main/projeto-integradorDeploy-main/projeto-interador-main/src/main/java/com/example/demo/MHMovieRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MHMovieRepository extends JpaRepository<MHMovieReserva, Long> {
-}
--- Downloads/INTEGRADOR2-main/INTEGRADOR2-main/projeto-integradorDeploy-main/projeto-integradorDeploy-main/projeto-interador-main/src/main/java/com/example/demo/dto.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo;
-
-public record dto(String nome,String horarioReserva,String chaveReserva,String imagemPerfil,String filmeEscolhido) {
-
-}
